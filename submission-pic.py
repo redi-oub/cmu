98a0a4fd43d1853e25f568e7a264159c58e625bd
redi-oub <ben.ou@lseg.com>
2026-04-19 20:27:00 -0400

    Add files via upload

--- submission-pic.py
+++ submission-pic.py
@@ -1,4 +1,4 @@
-"""test3b: test2 + receive_messages extracting value + use in recovery. NO init computation."""
+"""test3b + vis_means + neighbor avg + quadrant avgs."""
 import sys
 
 # Try every possible way to get Strategy
@@ -43,12 +43,19 @@ class SubmissionStrategy(Strategy):
         super().__init__(corrupted)
         self.corrupted = corrupted
         self.missing = set()
+        self.vis_means = {}
         self.req_order = []
         self.recv_avgs = {}
         for br in range(5):
             for bc in range(5):
                 if corrupted[br * 10][bc * 10] is None:
                     self.missing.add((br, bc))
+                else:
+                    s = 0.0
+                    for r in range(br * 10, br * 10 + 10):
+                        for c in range(bc * 10, bc * 10 + 10):
+                            s += corrupted[r][c]
+                    self.vis_means[(br, bc)] = s / 100.0
 
     def make_requests(self):
         try:
@@ -57,10 +64,20 @@ class SubmissionStrategy(Strategy):
                 return []
             reqs = []
             order = []
-            for br, bc in sorted(self.missing):
+            sm = sorted(self.missing)
+            # Block averages
+            for br, bc in sm:
                 r1, c1 = br * 10, bc * 10
                 reqs.append(RegionAverageRequest(r1, c1, r1 + 9, c1 + 9))
-                order.append((br, bc))
+                order.append(('avg', br, bc))
+            # Quadrant averages
+            for br, bc in sm:
+                r1, c1 = br * 10, bc * 10
+                for qr1, qc1, qr2, qc2 in [
+                    (r1, c1, r1+4, c1+4), (r1, c1+5, r1+4, c1+9),
+                    (r1+5, c1, r1+9, c1+4), (r1+5, c1+5, r1+9, c1+9)]:
+                    reqs.append(RegionAverageRequest(qr1, qc1, qr2, qc2))
+                    order.append(('qavg', br, bc, qr1, qc1, qr2, qc2))
             self.req_order = order
             return reqs
         except Exception:
@@ -75,8 +92,14 @@ class SubmissionStrategy(Strategy):
                 if msg is None or i >= len(self.req_order):
                     continue
                 val = getattr(msg, 'value', None)
-                if val is not None:
-                    self.recv_avgs[self.req_order[i]] = float(val)
+                if val is None:
+                    continue
+                val = float(val)
+                m = self.req_order[i]
+                if m[0] == 'avg':
+                    self.recv_avgs[(m[1], m[2])] = val
+                elif m[0] == 'qavg':
+                    self.recv_avgs[(m[3], m[4], m[5], m[6])] = val
         except Exception:
             pass
 
@@ -90,7 +113,38 @@ class SubmissionStrategy(Strategy):
                 if v is not None:
                     row.append(v)
                 else:
-                    avg = self.recv_avgs.get((r // 10, c // 10))
-                    row.append(avg if avg is not None else 0.5)
+                    br, bc = r // 10, c // 10
+                    # Try quadrant avg first
+                    r1, c1 = br * 10, bc * 10
+                    qavg = None
+                    for qr1, qc1, qr2, qc2 in [
+                        (r1, c1, r1+4, c1+4), (r1, c1+5, r1+4, c1+9),
+                        (r1+5, c1, r1+9, c1+4), (r1+5, c1+5, r1+9, c1+9)]:
+                        if qr1 <= r <= qr2 and qc1 <= c <= qc2:
+                            qavg = self.recv_avgs.get((qr1, qc1, qr2, qc2))
+                            break
+                    if qavg is not None:
+                        row.append(qavg)
+                    else:
+                        avg = self.recv_avgs.get((br, bc))
+                        if avg is not None:
+                            row.append(avg)
+                        else:
+                            row.append(self._neighbor_avg(br, bc))
             result.append(row)
         return result
+
+    def _neighbor_avg(self, br, bc):
+        total, cnt = 0.0, 0
+        for dbr in (-1, 0, 1):
+            for dbc in (-1, 0, 1):
+                if dbr == 0 and dbc == 0:
+                    continue
+                nb = (br + dbr, bc + dbc)
+                if nb in self.vis_means:
+                    total += self.vis_means[nb]
+                    cnt += 1
+                elif nb in self.recv_avgs:
+                    total += self.recv_avgs[nb]
+                    cnt += 1
+        return total / cnt if cnt > 0 else 0.5
